da901f3d470546ee1e782c9ae776f7cb28e9ea21
Robert Harris <rharris@gogoair.com>
2015-05-21 21:27:13 +0000

    Re-write to use the sub procs to walk by the hour and recurse into the sub-hash's.

--- Get_Palladion.pl
+++ Get_Palladion.pl
@@ -1,24 +1,28 @@
 #!/usr/bin/perl -w
 
+$|=1;
+
+use strict;
 use LWP::Simple;
 use JSON qw( decode_json );
+use XML::Simple;
 use Data::Dumper;
-use strict;
+use Scalar::Util qw/reftype/;
 
+# Get yesterday's data
 my $TargetDate=`/bin/date --date="yesterday" +%Y-%m-%d`;  chomp( $TargetDate );
+# Get a specific date
+#my $TargetDate="2015-05-19";
+
+# Example URL Info
 # admin:oracle http://10.240.21.100/r/calls/2015-03-16+00:00:00/2015-03-16+23:55:00
-my $User="admin:oracle";
-#my $TargetEx="http://$User\@10.240.21.100/r/calls/2015-03-16+00:00:00/2015-03-16+23:55:00";
-my $TargetBase="http://10.240.21.100/r/calls";
-my $TargetStart="$TargetDate+00:00:00";
-my $TargetStop="$TargetDate+23:59:59";
-my $Target=$TargetBase."/".$TargetStart."/".$TargetStop;
-my $JSONOUT;
+my $JSONString;
 my $Call;
 my %Calls;
 my $Counter=0;
-my $MainCounter=0;
 my $Key;
+my $Leg;
+my $Verbose=0;
 
 # Target Ends
 #  /vq - Voice Quality
@@ -29,131 +33,140 @@ my $Key;
 
 # Works
 
-&Get_Calls;
+my $Hour=0;
+
+foreach my $Hour ( 0..23 ) {
+  &Get_Calls($Hour);
+}
 
 exit 0;
 
 #
 sub Get_Calls {
-  my $JSON = new JSON;
+  my $Hour=$_[0];
+  $Hour=substr("0".$Hour, -2);
+
+  my $User="admin:oracle";
+  my $TargetBase="http://10.240.21.100/r/calls";
+  my $TargetStart="$TargetDate+".$Hour.":00:00";
+  my $TargetStop="$TargetDate+".$Hour.":59:59";
+  my $Target=$TargetBase."/".$TargetStart."/".$TargetStop;
+
+  print "Retrieving hour $Hour: $Target\n";
+
+  my $Converter = new JSON;
   print "Getting json $Target\n";
   open(INPUT, "/usr/bin/curl -k -L --digest -s --user $User $Target |");
   while(<INPUT>) {
     chomp;
-    $JSONOUT=$_;
+    $JSONString=$_;
   }
-#print Dumper $JSONOUT."\n";
 
-  my $json_text = $JSON->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONOUT);
+
+  #my $TopXML = $Converter->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONString);
+  my $TopXML = $Converter->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONString) || die "Can't decode $JSONString :$!:\n";
+
+  my $XMLOut = XMLout( $TopXML, SuppressEmpty => undef, KeyAttr => { data => 'call_id'} );
+  my $XML = XMLin( $XMLOut, SuppressEmpty => undef, KeyAttr => { data => 'call_id'} );
+
+  foreach $Call ( keys( %{$XML->{data}} ) ) {
+    print "Main Call :$Call:\n" if ( $Verbose );
+    my $Leg=0;
+
+    # Call Details is the key for the part ( i.e. setup_time )
+    foreach my $CallDetails ( keys( %{$XML->{data}->{$Call}} ) ) {
+      next unless ( ${$XML->{data}->{$Call}}{$CallDetails} );
+      $Key=$Call.".".$Leg.".".$CallDetails;
+      print "$Key : ${$XML->{data}{$Call}}{$CallDetails}\n";
+    }
+
+    &Get_VQ( $Call, ${$XML->{data}->{$Call}}{"url"});
+# This limits it to just the first call...
+#exit 0;
+
+  }
   
-
-  $MainCounter="000";
-  # Loop through the total call list
-  foreach my $Loop (  @{$json_text->{data}} ) {
-    my %Loop2=%{$Loop};
-
-    # Loop through the individual keys in this call
-    foreach my $Loop2 ( keys ( %{$Loop} ) ) {
-#      print "  \$Loop2{$Loop2} :$Loop2{$Loop2}:\n";
-      $Key="$MainCounter"."-"."$Loop2";
-#      $Calls{"$MainCounter-$Loop2"}=$Loop2{$Loop2} if ( $Loop2{$Loop2} );
-      $Calls{"$Key"}=$Loop2{$Loop2} if ( $Loop2{$Loop2} );
-    }
-
-    $Key="$MainCounter"."-code";
-    if ( $Calls{"$Key"} != "603" ) {
-      print "Lookign Deeper.\n";
-      $Key="$MainCounter"."-url";
-      &Get_Call_VQ($Calls{"$MainCounter-url"});
-    } else {
-      next;
-    }
-
-#   while (my($k, $v) = each( %Calls ) ){
-#     print "$k => $v\n";
-#   }
-    foreach my $PLoop ( sort ( keys %Calls ) ) {
-      if ( $PLoop =~ /^\d\d\d\d+-/ ) {
-        print "  $PLoop $Calls{$PLoop}\n";
-      } else {
-        print "$PLoop $Calls{$PLoop}\n";
-      }
-    }
-
-    print "\n";
-    %Calls = ();
-
-    $MainCounter++;
-    $MainCounter=substr("00".$MainCounter, -3);
-    exit 0;
-  }
 }
 
-sub Get_Call_Details {
-  my $TargetURL=$_[0];
-  my $TargetBase="http://10.240.21.100";
-  my $Target=$TargetBase.$TargetURL;
-  my $JSONOUT;
 
-  my $JSON = new JSON;
 
-  print "Getting json $Target\n";
+sub Get_VQ {
+  my $Call=$_[0];
+  my $URL=$_[1];
+  print "\$URL :$URL:\n" if ( $Verbose );
+
+  my $User="admin:oracle";
+  my $TargetBase="http://10.240.21.100/";
+  my $Target=$TargetBase."/".$URL;
+
+
+  my $Converter = new JSON;
+  print "Getting json $Target\n" if ( $Verbose );
+  print "/usr/bin/curl -k -L --digest -s --user $User $Target\n" if ( $Verbose );
   open(INPUT, "/usr/bin/curl -k -L --digest -s --user $User $Target |");
   while(<INPUT>) {
     chomp;
-    $JSONOUT=$_;
+    $JSONString=$_;
+
+    print "\$JSONString :$JSONString:\n" if ( $Verbose );
   }
 
-  my $json_text = $JSON->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONOUT);
- 
-  my @Keys=keys %{ $json_text };
-#  foreach my $Loop ( @Keys ) {
-#    print "\${\$json_text}{$Loop} :${$json_text}{$Loop}:\n";
-#  }
-}
+  my $TopXML = $Converter->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONString) || die "Can't decode $JSONString :$!:\n";
 
+  my $XMLOut = XMLout( $TopXML, SuppressEmpty => undef, KeyAttr => { data => 'call_id'} );
+  my $XML = XMLin( $XMLOut, SuppressEmpty => undef, KeyAttr => { data => 'call_id'} );
 
-sub Get_Call_VQ {
-  my $TargetURL=$_[0];
-  my $TargetBase="http://10.240.21.100";
-  my $Target=$TargetBase.$TargetURL."/vq";
-  my $JSONOUT;
-  
-  my $JSON = new JSON;
+  print Dumper $XML if ( $Verbose );
 
-  print "Getting VQ json $Target\n";
-  open(INPUT, "/usr/bin/curl -k -L --digest -s --user $User $Target |");
-  while(<INPUT>) {
-    chomp;
-    $JSONOUT=$_;
-  }
-
-  my $json_text = $JSON->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONOUT);
-
-  $Counter=1;
-  foreach my $Loop ( @{$json_text->{data}} ) {
-    my %Loop2=%{$Loop};
-    foreach my $Loop2 ( keys ( %{$Loop} ) ) {
-#      $Calls{"$MainCounter-$Counter-$Loop2"}=$Loop2{$Loop2} if ( $Loop2{$Loop2} );
-      $Calls{"$MainCounter$Counter-$Loop2"}=$Loop2{$Loop2} if ( $Loop2{$Loop2} );
+  foreach my $VQ ( keys( %{$XML} ) ) {
+    if ( ${$XML}{$VQ} ) {
+      &Process_Value( $Call, ${$XML}{$VQ} );
     }
+  }
+}
 
-    &Get_Call_VQ_Details($Loop2{"data"}) if ( $Loop2{"data"} );
-    $Counter++;
+#
+# Processors here!
+#
+sub Process_Value {
+  my $Call=$_[0];
+  my $Ref=$_[1];
+
+  my $RefType = reftype $Ref;
+  if ( ! $RefType ) {
+    print "VQ $Ref\n" if ( $Verbose );
+  } elsif ( $RefType eq "HASH" ) {
+    print "Found $RefType at $Ref\n" if ( $Verbose );
+    &Walk_Hash( $Call, $Ref);
+  } elsif ( $RefType eq "ARRAY" ) {
+    print "Found $RefType at $Ref\n" if ( $Verbose );
+    &Walk_Array( $Call, $Ref);
   }
 }
 
 
-sub Get_Call_VQ_Details {
+sub Walk_Array {
+  my $Call=$_[0];
+  my $Ref=$_[1];
 
-  my $Ref=$_[0];
+  print "  Processing Array $Ref\n" if ( $Verbose );
+  foreach my $Loop ( @{$Ref} ) {
+    print "  \$Loop :$Loop:\n" if ( $Verbose );
+    &Process_Value( $Call, $Loop );
+  }
 
-#print Dumper $Ref;
-  
-  my %Loop2=%{$Ref};
-  foreach my $Loop2 ( sort ( keys ( %{$Ref} ) ) ) {
-#    print "  \$Loop2{$Loop2} :$Loop2{$Loop2}:\n";
-#    $Calls{"$MainCounter-$Counter-$Loop2"}=$Loop2{$Loop2} if ( $Loop2{$Loop2} );
-    $Calls{"$MainCounter$Counter-$Loop2"}=$Loop2{$Loop2} if ( $Loop2{$Loop2} );
+}
+
+
+sub Walk_Hash {
+  my $Call=$_[0];
+  my $Ref=$_[1];
+
+  print "  Processing Hash $Ref\n" if ( $Verbose );
+  $Leg++ if ( ${$Ref}{"src_device_name"} );
+  print " ** Leg $Leg\n" if ( $Verbose );
+  foreach my $Loop ( keys ( %{$Ref} ) ) {
+    $Key=$Call.".".$Leg.".".$Loop;
+    print "    $Key : ${$Ref}{$Loop}\n" if ( ${$Ref}{$Loop} );
   }
 }
