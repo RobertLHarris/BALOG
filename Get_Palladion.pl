33b4d1016addc68548928592016fd761b828230c
root <root@pil-vm-balog-11.aircell.prod>
2015-06-08 19:42:25 +0000

    Re-write.  Now walks JSON intelligently.

--- Get_Palladion.pl
+++ Get_Palladion.pl
@@ -12,7 +12,11 @@ use Scalar::Util qw/reftype/;
 # Get yesterday's data
 my $TargetDate=`/bin/date --date="yesterday" +%Y-%m-%d`;  chomp( $TargetDate );
 # Get a specific date
-#my $TargetDate="2015-05-19";
+my @TargetDates=( "2015-05-01", "2015-05-02", "2015-05-03", "2015-05-04", "2015-05-05", "2015-05-06", "2015-05-07", "2015-05-08", "2015-05-09",
+		  "2015-05-10", "2015-05-11", "2015-05-12", "2015-05-13", "2015-05-14", "2015-05-15", "2015-05-16", "2015-05-17", "2015-05-18", "2015-05-19",
+		  "2015-05-20", "2015-05-21", "2015-05-22", "2015-05-23", "2015-05-24", "2015-05-25", "2015-05-26", "2015-05-27", "2015-05-28", "2015-05-29",
+		  "2015-05-30", "2015-05-30", "2015-05-31",
+		  "2015-06-01", "2015-06-02", "2015-06-03", "2015-06-04", "2015-06-05", "2015-06-06", "2015-06-07", "2015-06-08" );
 
 # Example URL Info
 # admin:oracle http://10.240.21.100/r/calls/2015-03-16+00:00:00/2015-03-16+23:55:00
@@ -23,6 +27,8 @@ my $Counter=0;
 my $Key;
 my $Leg;
 my $Verbose=0;
+my $Testing=0;
+my $Printout=1;
 
 # Target Ends
 #  /vq - Voice Quality
@@ -31,12 +37,11 @@ my $Verbose=0;
 #  /r/counters - The list of counters for the admin user and the `ALL' realm. 
 #  /r/counters/<id> A representation of the given counter. 
 
-# Works
-
-my $Hour=0;
-
-foreach my $Hour ( 0..23 ) {
-  &Get_Calls($Hour);
+foreach $TargetDate ( @TargetDates ) {
+  print "Pulling Data for $TargetDate.\n";
+  foreach my $Hour ( 0..23 ) {
+    &Get_Calls($Hour);
+  }
 }
 
 exit 0;
@@ -52,10 +57,10 @@ sub Get_Calls {
   my $TargetStop="$TargetDate+".$Hour.":59:59";
   my $Target=$TargetBase."/".$TargetStart."/".$TargetStop;
 
-  print "Retrieving hour $Hour: $Target\n";
+  print "Retrieving hour $Hour: $Target\n" if ( $Verbose );
 
   my $Converter = new JSON;
-  print "Getting json $Target\n";
+  print "Getting json $Target\n" if ( $Verbose );
   open(INPUT, "/usr/bin/curl -k -L --digest -s --user $User $Target |");
   while(<INPUT>) {
     chomp;
@@ -63,34 +68,35 @@ sub Get_Calls {
   }
 
 
-  #my $TopXML = $Converter->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONString);
-  my $TopXML = $Converter->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONString) || die "Can't decode $JSONString :$!:\n";
+  my $SRC = $Converter->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONString) || die "Can't decode $JSONString :$!:\n";
 
-  my $XMLOut = XMLout( $TopXML, SuppressEmpty => undef, KeyAttr => { data => 'call_id'} );
-  my $XML = XMLin( $XMLOut, SuppressEmpty => undef, KeyAttr => { data => 'call_id'} );
+  # This creates a REF to an array of each call's data.
+  #  @{$DATA}[0]->{call_id} would have the ID for the first call
+  my $DATA=${$SRC}{data};
 
-  foreach $Call ( keys( %{$XML->{data}} ) ) {
+  foreach my $Loop ( @{$DATA} ) {
+    $Call=${$Loop}{call_id};
     print "Main Call :$Call:\n" if ( $Verbose );
-    my $Leg=0;
+    $Leg=0;
 
     # Call Details is the key for the part ( i.e. setup_time )
-    foreach my $CallDetails ( keys( %{$XML->{data}->{$Call}} ) ) {
-      next unless ( ${$XML->{data}->{$Call}}{$CallDetails} );
+    foreach my $CallDetails ( keys( %{$Loop} ) ) {
+      next unless ( ${$Loop}{$CallDetails} );
       $Key=$Call.".".$Leg.".".$CallDetails;
-      print "$Key : ${$XML->{data}{$Call}}{$CallDetails}\n";
+      print "$Key : ${$Loop}{$CallDetails}\n" if ( $Printout );
     }
 
-    &Get_VQ( $Call, ${$XML->{data}->{$Call}}{"url"});
-# This limits it to just the first call...
-#exit 0;
+    &Get_Details( $Call, ${$Loop}{"url"});
+    &Get_VQ( $Call, ${$Loop}{"url"});
+
+   # This limits it to just the first call...
+   exit 0 if ( $Testing );
 
   }
-  
 }
 
 
-
-sub Get_VQ {
+sub Get_Details {
   my $Call=$_[0];
   my $URL=$_[1];
   print "\$URL :$URL:\n" if ( $Verbose );
@@ -111,17 +117,76 @@ sub Get_VQ {
     print "\$JSONString :$JSONString:\n" if ( $Verbose );
   }
 
-  my $TopXML = $Converter->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONString) || die "Can't decode $JSONString :$!:\n";
+  my $SRC = $Converter->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONString) || die "Can't decode $JSONString :$!:\n";
+  print Dumper $SRC if ( $Verbose );
 
-  my $XMLOut = XMLout( $TopXML, SuppressEmpty => undef, KeyAttr => { data => 'call_id'} );
-  my $XML = XMLin( $XMLOut, SuppressEmpty => undef, KeyAttr => { data => 'call_id'} );
-
-  print Dumper $XML if ( $Verbose );
-
-  foreach my $VQ ( keys( %{$XML} ) ) {
-    if ( ${$XML}{$VQ} ) {
-      &Process_Value( $Call, ${$XML}{$VQ} );
+  foreach my $VQ ( keys( %{$SRC} ) ) {
+    if ( ${$SRC}{$VQ} ) {
+      print "\${$SRC}{$VQ} :${$SRC}{$VQ}:\n" if ( $Verbose );
+      &Process_Value( $Call, ${$SRC}{$VQ} );
     }
+  }
+}
+
+
+sub Get_VQ {
+  my $Call=$_[0];
+  my $URL=$_[1];
+  $URL=$URL."/vq";
+  print "\$URL :$URL:\n" if ( $Verbose );
+  print "\$Call :$Call:\n" if ( $Verbose );
+  print "\$URL :$URL:\n" if ( $Verbose );
+
+  my $User="admin:oracle";
+  my $TargetBase="http://10.240.21.100/";
+  my $Target=$TargetBase."/".$URL;
+
+
+  my $Converter = new JSON;
+  open(INPUT, "/usr/bin/curl -k -L --digest -s --user $User $Target |");
+  while(<INPUT>) {
+    chomp;
+    $JSONString=$_;
+    
+    print "\$JSONString :$JSONString:\n" if ( $Verbose );
+  }
+
+  my $SRC = $Converter->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($JSONString) || die "Can't decode $JSONString :$!:\n";
+  my $DATA=${$SRC}{data};
+  print Dumper $DATA if ( $Verbose );
+
+  foreach my $Loop ( @{$DATA} ) {
+    # Call Details is the key for the part ( i.e. setup_time )
+    foreach my $CallVQ ( keys( %{$Loop} ) ) {
+      my $Direction=${$Loop}{direction};
+      my $Value;
+      $Direction="Unknown" if ( ! $Direction );
+      if ( $CallVQ ne "data" ) {
+        next unless ( ${$Loop}{$CallVQ} );
+        $Key=$Call.".".$Leg.".".$Direction."-".$CallVQ;
+        print "$Key : ${$Loop}{$CallVQ}\n" if ( $Printout );
+      } else {
+        $Key=$Call.".".$Leg.".".$Direction."-moscque_avg";
+        $Value=${$Loop}{data}{moscqe_avg}; $Value="Unknown" if ( ! $Value );
+        print "$Key : $Value\n";
+        $Key=$Call.".".$Leg.".".$Direction."-packets_lost";
+        $Value=${$Loop}{data}{packets_lost}; $Value="Unknown" if ( ! $Value );
+        print "$Key : $Value\n";
+        $Key=$Call.".".$Leg.".".$Direction."-jitter_max";
+        $Value=${$Loop}{data}{jitter_max}; $Value="Unknown" if ( ! $Value );
+        print "$Key : $Value\n";
+        $Key=$Call.".".$Leg.".".$Direction."-jitter_avg";
+        $Value=${$Loop}{data}{jitter_avg}; $Value="Unknown" if ( ! $Value );
+        print "$Key : $Value\n";
+        $Key=$Call.".".$Leg.".".$Direction."-packets_received";
+        $Value=${$Loop}{data}{packets_received}; $Value="Unknown" if ( ! $Value );
+        print "$Key : $Value\n";
+        $Key=$Call.".".$Leg.".".$Direction."-r_factor";
+        $Value=${$Loop}{data}{r_factor}; $Value="Unknown" if ( ! $Value );
+        print "$Key : $Value";
+      }
+    }
+
   }
 }
 
@@ -162,11 +227,38 @@ sub Walk_Hash {
   my $Call=$_[0];
   my $Ref=$_[1];
 
+  print "Hash 1\n" if ( $Verbose );
   print "  Processing Hash $Ref\n" if ( $Verbose );
   $Leg++ if ( ${$Ref}{"src_device_name"} );
   print " ** Leg $Leg\n" if ( $Verbose );
   foreach my $Loop ( keys ( %{$Ref} ) ) {
     $Key=$Call.".".$Leg.".".$Loop;
-    print "    $Key : ${$Ref}{$Loop}\n" if ( ${$Ref}{$Loop} );
+    next unless ( ${$Ref}{$Loop} );
+    my $RefType = reftype ${$Ref}{$Loop};
+    if ( ! $RefType ) {
+      print "$Key : ${$Ref}{$Loop}\n" if ( $Printout );
+    } elsif ( $RefType eq "HASH" ) {
+      print "Found $RefType at \${$Ref}{$Loop} - ${$Ref}{$Loop}\n" if ( $Verbose );
+      &Walk_Hash( $Call, ${$Ref}{$Loop});
+    } elsif ( $RefType eq "ARRAY" ) {
+      print "Found $RefType at \${$Ref}{$Loop} - ${$Ref}{$Loop}\n" if ( $Verbose );
+      &Walk_Array( $Call, ${$Ref}{$Loop});
+    }
+  }
+}
+
+
+sub Walk_Hash2 {
+  my $Call=$_[0];
+  my $Type=$_[1];
+  my $Ref=$_[2];
+
+  print "Hash 2\n" if ( $Verbose );
+  print "  Processing Hash $Ref\n" if ( $Verbose );
+  $Leg++ if ( ${$Ref}{"src_device_name"} );
+  print " ** Leg $Leg\n" if ( $Verbose );
+  foreach my $Loop ( keys ( %{$Ref} ) ) {
+    $Key=$Call.".".$Leg.".".$Type.".".$Loop;
+    print "$Key : ${$Ref}{$Loop}\n" if ( ( ${$Ref}{$Loop} ) && ( $Printout ) );
   }
 }
